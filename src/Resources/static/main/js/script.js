6d8ec6a4c0db6a5c8d1cd7d3593a59a76b828153
cnam <cnam812@gmail.com>
2015-04-13 15:38:21 +0300

    fixed multi scroll

--- src/Resources/static/main/js/script.js
+++ src/Resources/static/main/js/script.js
@@ -1,4 +1,4 @@
-function show_description(id){
+function show_description(id) {
 
     window.location.hash = id+'_desc_GET';
 
@@ -30,4 +30,33 @@ function show_description(id){
     if (current) {
         current.className = 'active_tab';
     }
+
+    $(document).on('DOMMouseScroll mousewheel', '.desc_resources', function(ev){
+        console.log(123);
+        var $this = $(this),
+            scrollTop = this.scrollTop,
+            scrollHeight = this.scrollHeight,
+            height = $this.height(),
+            delta = (ev.type == 'DOMMouseScroll' ?
+            ev.originalEvent.detail * -40 :
+                ev.originalEvent.wheelDelta),
+            up = delta > 0;
+
+        var prevent = function() {
+            ev.stopPropagation();
+            ev.preventDefault();
+            ev.returnValue = false;
+            return false;
+        };
+
+        if (!up && -delta > scrollHeight - height - scrollTop) {
+            // Scrolling down, but this will take us past the bottom.
+            $this.scrollTop(scrollHeight);
+            return prevent();
+        } else if (up && delta > scrollTop) {
+            // Scrolling up, but this will take us past the top.
+            $this.scrollTop(0);
+            return prevent();
+        }
+    });
 }
