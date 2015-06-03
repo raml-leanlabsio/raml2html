3d35b68087f61f238bf9f401cb150c1e747e2b9b
cnam <cnam812@gmail.com>
2015-06-04 00:42:00 +0300

    update phar version

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Application;
 $command = new \Cnam\Command\GenerateCommand();
 $watch = new \Cnam\Command\WatchCommand();
 
-$application = new Application('raml2html', '0.0.2');
+$application = new Application('raml2html', '0.0.4');
 
 $application->add($command);
 $application->add($watch);
