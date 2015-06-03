699cb646c4ada4fa0ac9f42ddbe8bfe6313d8f27
cnam <cnam812@gmail.com>
2015-06-04 01:11:11 +0300

    fixed bug for mediatype json

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Application;
 $command = new \Cnam\Command\GenerateCommand();
 $watch = new \Cnam\Command\WatchCommand();
 
-$application = new Application('raml2html', '0.0.4');
+$application = new Application('raml2html', '0.0.5');
 
 $application->add($command);
 $application->add($watch);
